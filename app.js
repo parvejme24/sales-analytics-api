6a2522e8eabfa6598436f050b1dd0b148f0ecaf1
Md Parvej <mdparvejme24@gmail.com>
2024-01-06 20:15:06 +0600

    install dotenv package and use this my application

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const salesRoutes = require("./routes/sales.routes");
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoose
