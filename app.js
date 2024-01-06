5680c642e258bd91f02e778bea04c2c8c6af90de
Md Parvej <mdparvejme24@gmail.com>
2024-01-06 20:11:42 +0600

    creae app.js file and organize code

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const salesRoutes = require("./routes/sales.routes");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+
+// Middlewares
+app.use(express.json());
+
+// Routes
+app.use("/api/sales", salesRoutes);
+
+// Start server
+module.exports = app;
--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const salesRoutes = require('./routes/sales.routes');
+const app = require("./app");
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
-
-// Middlewares
-app.use(express.json());
-
-// Routes
-app.use('/api/sales', salesRoutes);
-
-// Start server
 app.listen(port, () => console.log(`Server listening on port ${port}`));
