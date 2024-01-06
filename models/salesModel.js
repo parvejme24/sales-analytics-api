bdfe82ba8922d3c23bff79686ca0f76c6c709e83
Md Parvej <mdparvejme24@gmail.com>
2024-01-06 20:06:46 +0600

    first commit

---
+++ models/salesModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const salesSchema = new mongoose.Schema({
+  product: {
+    type: String,
+    required: true
+  },
+  quantity: {
+    type: Number,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Sale', salesSchema);
