0397f92f753d9a313746b2b66736fc7a71c9f20d
Md Parvej <mdparvejme24@gmail.com>
2024-01-06 20:17:26 +0600

    update some code in salesRoutes.js file

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Sale = require('../models/sales.model');
+const Sale = require("../models/salesModel");
 
-// Implement each API endpoint using MongoDB aggregation framework
-// For example:
-
-router.get('/total-revenue', async (req, res) => {
+router.get("/total-revenue", async (req, res) => {
   try {
     const totalRevenue = await Sale.aggregate([
-      { $group: { _id: null, totalRevenue: { $sum: { $multiply: ['$quantity', '$price'] } } } }
+      {
+        $group: {
+          _id: null,
+          totalRevenue: { $sum: { $multiply: ["$quantity", "$price"] } },
+        },
+      },
     ]).project({ _id: 0 });
 
     res.json(totalRevenue);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error calculating total revenue');
+    res.status(500).send("Error calculating total revenue");
   }
 });
-
-// ... implement other endpoints similarly
 
 module.exports = router;
